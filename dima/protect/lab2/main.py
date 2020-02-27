0658099ba972b90f7ca575c9d79a960910fa9805
dekeyel <dekeyel>
2020-02-27 11:59:27 +0300

    ref romanenko lab2 no done

--- dima/protect/lab2/main.py
+++ dima/protect/lab2/main.py
@@ -1,13 +1,17 @@
+import sympy
+
 class Alph:
     def __init__(self):
         self. alph = '01'
 
     def razl(self, sump):
-        mass = [None, sump]
-        for i in range(13):
-            mass.append(sump * (len(mass[-1]) + 1))
-
-        return mass
+        res = sympy.Symbol('E')
+        step = sump
+        for i in range(1):
+            res += step
+            step *= sump
+        #print(res)
+        return res
 
     def comp(self, left, right):
         if not(left and right):
@@ -16,8 +20,15 @@ class Alph:
             return left + right
 
     def chet(self):
-        mass = ['S0Z', '1', '0Z', '1', '0ZSZ']
-        print(''.join(mass))
+        x = sympy.Symbol('0')
+        y = sympy.Symbol('1')
+        a = self.razl(x) + y + self.razl(x) + y + self.razl(x)
+        sympy.pprint(a)
+        ish = self.razl(a).expand()
+        sympy.pprint(ish)
 
 if __name__ == '__main__':
-    print(Alph().chet())
+    x = sympy.Symbol('x')
+    y = sympy.Symbol('y')
+    a = 1 + x + y
+    Alph().chet()
